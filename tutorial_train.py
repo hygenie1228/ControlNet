ac35b20a8d0aea79243d8191050e2687a35864d8
hygenie1228 <namhj28@gmail.com>
2024-05-02 07:25:01 +0000

    tmp

--- tutorial_train.py
+++ tutorial_train.py
@@ -1,18 +1,18 @@
 import sys
 import os
 import os.path as osp
-this_dir = osp.dirname(__file__)
-path = osp.join(this_dir, '..')
-if path not in sys.path: sys.path.insert(0, path)
 
 from share import *
 import datetime
 
 import pytorch_lightning as pl
+from pytorch_lightning import loggers as pl_loggers
+from pytorch_lightning.callbacks import ModelCheckpoint
 from torch.utils.data import DataLoader
 from tutorial_dataset import MyDataset
 from cldm.logger import ImageLogger
 from cldm.model import create_model, load_state_dict
+
 
 
 # Configs
@@ -44,10 +44,11 @@ os.system(f'cp -r lib {save_codes_path}')
 
 # Misc
 dataset = MyDataset()
-dataloader = DataLoader(dataset, num_workers=0, batch_size=batch_size, shuffle=True)
+dataloader = DataLoader(dataset, num_workers=4, batch_size=batch_size, shuffle=True)
 logger = ImageLogger(batch_frequency=logger_freq)
-trainer = pl.Trainer(gpus=1, precision=32, callbacks=[logger])
-
+tb_logger = pl_loggers.TensorBoardLogger(save_dir=save_folder_path, name='controlnet')
+ckpt_callback = ModelCheckpoint(every_n_train_steps=5000, save_top_k=-1) # ModelCheckpoint(every_n_epochs=2, save_top_k=-1)
+trainer = pl.Trainer(max_epochs=5, gpus=1, precision=32, logger=tb_logger, callbacks=[ckpt_callback, logger])
 
 # Train!
 trainer.fit(model, dataloader)
